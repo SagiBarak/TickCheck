8edc2ce5a18b629dabadb50b217f218d2028d8b9
sagib <sagi1506@gmail.com>
2017-06-16 09:03:06 +0300

    updated version

--- app/src/main/java/sagib/edu/tickcheck/ShowAdapter.java
+++ app/src/main/java/sagib/edu/tickcheck/ShowAdapter.java
@@ -1,22 +1,17 @@
 package sagib.edu.tickcheck;
 
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
