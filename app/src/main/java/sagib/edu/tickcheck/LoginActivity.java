8fbb38e8a0d08feccbfa72a69b2581f58a0a82d2
sagib <sagi1506@gmail.com>
2017-06-18 17:39:36 +0300

    updated version

---
+++ app/src/main/java/sagib/edu/tickcheck/LoginActivity.java
@@ -0,0 +1,134 @@
+package sagib.edu.tickcheck;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    String email;
+    String password;
+    String firstName;
+    String lastName;
+
+    @BindView(R.id.ivLoginLogo)
+    ImageView ivLoginLogo;
+    @BindView(R.id.etUserName)
+    EditText etUserName;
+    @BindView(R.id.tilUserName)
+    TextInputLayout tilUserName;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+    @BindView(R.id.tilPassword)
+    TextInputLayout tilPassword;
+    @BindView(R.id.btnSignIn)
+    BootstrapButton btnSignIn;
+    @BindView(R.id.btnRegister)
+    BootstrapButton btnRegister;
+    @BindView(R.id.tvRegDetailsTitle)
+    TextView tvRegDetailsTitle;
+    @BindView(R.id.etFirstName)
+    EditText etFirstName;
+    @BindView(R.id.tilFirstName)
+    TextInputLayout tilFirstName;
+    @BindView(R.id.etLastName)
+    EditText etLastName;
+    @BindView(R.id.tilLastName)
+    TextInputLayout tilLastName;
+    @BindView(R.id.btnFinalRegister)
+    BootstrapButton btnFinalRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        tvRegDetailsTitle.setVisibility(View.GONE);
+        etFirstName.setVisibility(View.GONE);
+        tilFirstName.setVisibility(View.GONE);
+        etLastName.setVisibility(View.GONE);
+        tilLastName.setVisibility(View.GONE);
+        btnFinalRegister.setVisibility(View.GONE);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @OnClick(R.id.btnSignIn)
+    public void onBtnSignInClicked() {
+        email = etUserName.getText().toString();
+        password = etPassword.getText().toString();
+        if (!email.isEmpty() && !password.isEmpty()) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            goToMain();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(LoginActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    @OnClick(R.id.btnRegister)
+    public void onBtnRegisterClicked() {
+        btnSignIn.animate().alpha(0).rotation(360);
+        btnSignIn.setVisibility(View.GONE);
+        btnRegister.animate().alpha(0).rotation(360);
+        btnRegister.setVisibility(View.GONE);
+        tvRegDetailsTitle.setVisibility(View.VISIBLE);
+        etFirstName.setVisibility(View.VISIBLE);
+        tilFirstName.setVisibility(View.VISIBLE);
+        etLastName.setVisibility(View.VISIBLE);
+        tilLastName.setVisibility(View.VISIBLE);
+        btnFinalRegister.setVisibility(View.VISIBLE);
+
+    }
+
+    @OnClick(R.id.btnFinalRegister)
+    public void onBtnFinalRegisterClicked() {
+        email = etUserName.getText().toString();
+        password = etPassword.getText().toString();
+        firstName = etFirstName.getText().toString();
+        lastName = etLastName.getText().toString();
+        if (!email.isEmpty() && !password.isEmpty() && !firstName.isEmpty() && !lastName.isEmpty()) {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    goToMain();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(LoginActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    public void goToMain() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/sagib/edu/tickcheck/MainActivity.java
+++ app/src/main/java/sagib/edu/tickcheck/MainActivity.java
@@ -1,7 +1,9 @@
 package sagib.edu.tickcheck;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +16,9 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.ArrayList;
 
 import static android.view.View.GONE;
@@ -21,11 +26,34 @@ import static android.view.View.GONE;
 public class MainActivity extends AppCompatActivity implements ShowDataSource.OnShowArrivedListener {
     RecyclerView recycler;
     private ProgressDialog dialog;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private boolean userConnected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    userConnected = false;
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                } else {
+                    userConnected = true;
+                    dialog = new ProgressDialog(MainActivity.this);
+                    dialog.setMessage("נא להמתין, מרענן רשימת הופעות..." + "\n" + "על מנת לחסוך בשימוש חבילת נתונים," + "\n" + "מומלץ להתחבר לרשת אלחוטית.");
+                    dialog.setCancelable(false);
+                    dialog.setCanceledOnTouchOutside(false);
+                    dialog.show();
+                }
+            }
+        };
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -33,11 +61,6 @@ public class MainActivity extends AppCompatActivity implements ShowDataSource.On
         recycler = (RecyclerView) findViewById(R.id.recycler);
         ShowDataSource.getShows(this);
         recycler.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        dialog = new ProgressDialog(this);
-        dialog.setMessage("נא להמתין, מרענן רשימת הופעות..." + "\n" + "על מנת לחסוך בשימוש חבילת נתונים," + "\n" + "מומלץ להתחבר לרשת אלחוטית.");
-        dialog.setCancelable(false);
-        dialog.setCanceledOnTouchOutside(false);
-        dialog.show();
     }
 
     @Override
@@ -59,6 +82,9 @@ public class MainActivity extends AppCompatActivity implements ShowDataSource.On
             dialog.show();
             ShowDataSource.getShows(this);
         }
+        if (id == R.id.action_signout){
+            mAuth.signOut();
+        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -71,12 +97,27 @@ public class MainActivity extends AppCompatActivity implements ShowDataSource.On
                 if (e == null) {
                     ShowAdapter adapter = new ShowAdapter(data, MainActivity.this);
                     recycler.setAdapter(adapter);
-                    dialog.dismiss();
+                    if (userConnected)
+                        dialog.dismiss();
                 } else {
                     Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
                     Log.d("Sagi", e.toString());
                 }
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
