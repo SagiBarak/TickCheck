8796000415be1f7e8b1631781f462f3d79a9e306
sagib <sagi1506@gmail.com>
2017-08-05 13:49:44 +0300

    updated version

--- app/src/main/java/sagib/edu/tickcheck/ShowAdapter.java
+++ app/src/main/java/sagib/edu/tickcheck/ShowAdapter.java
@@ -1,36 +1,21 @@
 package sagib.edu.tickcheck;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class ShowAdapter extends RecyclerView.Adapter<ShowAdapter.ShowViewHolder> {
 
@@ -112,70 +97,6 @@ public class ShowAdapter extends RecyclerView.Adapter<ShowAdapter.ShowViewHolder
                     args.putParcelable("show", show);
                     showOptionsFragment.setArguments(args);
                     showOptionsFragment.show(fragment.getChildFragmentManager(), "ShowOptions");
-                }
-            });
-            v.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    int position = getAdapterPosition();
-                    final Show show = data.get(position);
-                    final AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                    builder.setTitle("הוסף לרשימת ההופעות שלי?").setMessage(String.format("האם להוסיף את ההופעה:\n%s ב%s\nב%s\nלרשימת המופעים שלי?", show.getPerformer(), show.getArena(), show.getDayDateTime()));
-                    builder.setPositiveButton("הוסף", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(final DialogInterface dialog, int which) {
-                            final String eventID = show.getLink().replace("https://tickets.zappa-club.co.il/loader.aspx/?target=hall.aspx?", "");
-                            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("MyShowsList").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
-                            reference.child(eventID).addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    if (dataSnapshot.getValue() != null) {
-                                        Toast.makeText(context, "ההופעה כבר קיימת ברשימה!", Toast.LENGTH_SHORT).show();
-                                        dialog.dismiss();
-                                    } else {
-                                        FirebaseDatabase.getInstance().getReference("MyShowsList").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(eventID).setValue(eventID);
-                                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("MyShows").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).push();
-                                        String date_s = show.getDateTime();
-                                        SimpleDateFormat dt = new SimpleDateFormat("dd.MM.yyyy HH:mm");
-                                        Date date = null;
-                                        try {
-                                            date = dt.parse(date_s);
-                                        } catch (ParseException e) {
-                                            e.printStackTrace();
-                                        }
-                                        SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
-                                        String finalDate = dt1.format(date);
-                                        MyShow myShow = new MyShow(show.getPerformer(), show.getDayDateTime(), show.getArena(), show.getImage(), ref.getKey(), finalDate, eventID);
-                                        ref.setValue(myShow).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                            @Override
-                                            public void onSuccess(Void aVoid) {
-                                                dialog.dismiss();
-                                                Toast.makeText(context, "ההופעה נוספה לרשימת ההופעות שלי", Toast.LENGTH_SHORT).show();
-                                            }
-                                        }).addOnFailureListener(new OnFailureListener() {
-                                            @Override
-                                            public void onFailure(@NonNull Exception e) {
-                                                dialog.dismiss();
-                                                Toast.makeText(context, "הפעולה נכשלה, נסה שנית", Toast.LENGTH_SHORT).show();
-                                            }
-                                        });
-                                    }
-                                }
-
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
-
-                                }
-                            });
-
-                        }
-                    }).setNegativeButton("בטל", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    }).show();
-                    return false;
                 }
             });
         }
